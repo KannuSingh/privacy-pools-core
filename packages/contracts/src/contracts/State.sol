f68b0602243085618748699eb5d712249fcb96ad
moebius <0xmoebius@protonmail.com>
2025-01-02 12:19:33 +0100

    chore: state contract natspec

--- packages/contracts/src/contracts/State.sol
+++ packages/contracts/src/contracts/State.sol
@@ -4,24 +4,35 @@ pragma solidity 0.8.28;
 import {InternalLeanIMT, LeanIMTData} from 'lean-imt/InternalLeanIMT.sol';
 
 import {IEntrypoint} from 'interfaces/IEntrypoint.sol';
+
 import {IState} from 'interfaces/IState.sol';
 import {IVerifier} from 'interfaces/IVerifier.sol';
 
-// TODO: implement cached roots
+/**
+ * @title State
+ * @notice Base contract for the state of a Privacy Pool
+ */
 abstract contract State is IState {
   using InternalLeanIMT for LeanIMTData;
 
+  /// @inheritdoc IState
   string public constant VERSION = '0.1.0';
 
+  /// @inheritdoc IState
   IEntrypoint public immutable ENTRYPOINT;
+  /// @inheritdoc IState
   IVerifier public immutable VERIFIER;
-  address private immutable _POSEIDON;
-
+  /// @inheritdoc IState
   uint256 public nonce;
+  /// @inheritdoc IState
   bool public dead;
+
+  address private immutable _POSEIDON;
   LeanIMTData internal _merkleTree;
 
-  mapping(uint256 _nullifierHash => bool _used) public nullifierHashes;
+  /// @inheritdoc IState
+  mapping(uint256 _nullifierHash => bool _spent) public nullifierHashes;
+  /// @inheritdoc IState
   mapping(uint256 _label => address _depositor) public labelToDepositor;
 
   modifier onlyEntrypoint() {
@@ -39,17 +50,35 @@ abstract contract State is IState {
                         INTERNAL METHODS
     //////////////////////////////////////////////////////////////*/
 
+  /**
+   * @notice Stores a nullifier hash as spent
+   * @param _nullifierHash The nullifier hash to spend
+   */
   function _spend(uint256 _nullifierHash) internal {
     if (nullifierHashes[_nullifierHash]) revert NullifierAlreadySpent();
     nullifierHashes[_nullifierHash] = true;
   }
 
-  function _insert(uint256 _root) internal {
-    _merkleTree._insert(_root);
+  /**
+   * @notice Inserts a leaf into the state
+   * @dev Reverts if the leaf is already in the state
+   * @param _leaf The leaf to insert
+   */
+  function _insert(uint256 _leaf) internal {
+    _merkleTree._insert(_leaf);
   }
 
+  /**
+   * @notice Returns whether the root is a known root
+   * @param _root The root to check
+   */
+  // TODO: implement cached roots
   function _isKnownRoot(uint256 _root) internal returns (bool) {}
 
+  /**
+   * @notice Returns whether a leaf is in the state
+   * @param _leaf The leaf to check
+   */
   function _isInState(uint256 _leaf) internal view returns (bool) {
     return _merkleTree._has(_leaf);
   }
--- packages/contracts/src/interfaces/IState.sol
+++ packages/contracts/src/interfaces/IState.sol
@@ -1,8 +1,78 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.28;
 
+import {IEntrypoint} from 'interfaces/IEntrypoint.sol';
+import {IVerifier} from 'interfaces/IVerifier.sol';
+
+/**
+ * @title IState
+ * @notice Interface for the State contract
+ */
 interface IState {
+  /*///////////////////////////////////////////////////////////////
+                              ERRORS
+    //////////////////////////////////////////////////////////////*/
+
+  /**
+   * @notice Thrown when trying to call a method only available to the Entrypoint
+   */
   error OnlyEntrypoint();
+
+  /**
+   * @notice Thrown when trying to deposit into a dead pool
+   */
   error PoolIsDead();
+
+  /**
+   * @notice Thrown when trying to spend a nullifier that has already been spent
+   */
   error NullifierAlreadySpent();
+
+  /*///////////////////////////////////////////////////////////////
+                              VIEWS 
+    //////////////////////////////////////////////////////////////*/
+
+  /**
+   * @notice Returns the version of the protocol
+   * @return _version The version string
+   */
+  function VERSION() external view returns (string memory _version);
+
+  /**
+   * @notice Returns the configured Entrypoint contract
+   * @return _entrypoint The Entrypoint contract
+   */
+  function ENTRYPOINT() external view returns (IEntrypoint _entrypoint);
+
+  /**
+   * @notice Returns the configured Verifier contract
+   * @return _verifier The Verifier contract
+   */
+  function VERIFIER() external view returns (IVerifier _verifier);
+
+  /**
+   * @notice Returns the current label nonce
+   * @return _nonce The current nonce
+   */
+  function nonce() external view returns (uint256 _nonce);
+
+  /**
+   * @notice Returns the boolean indicating if the pool is dead
+   * @return _dead The dead boolean
+   */
+  function dead() external view returns (bool _dead);
+
+  /**
+   * @notice Returns the spending status of a nullifier hash
+   * @param _nullifierHash The nullifier hash
+   * @return _spent The boolean indicating if the nullifier has been spent
+   */
+  function nullifierHashes(uint256 _nullifierHash) external view returns (bool _spent);
+
+  /**
+   * @notice Returns the original depositor that generated a label
+   * @param _label The label
+   * @return _depositor The original depositor
+   */
+  function labelToDepositor(uint256 _label) external view returns (address _depositor);
 }
