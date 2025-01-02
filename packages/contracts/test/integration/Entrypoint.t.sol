a897eda721c8ad86ad7e9f389c8a378583d33bae
moebius <0xmoebius@protonmail.com>
2025-01-02 15:35:57 +0100

    fix: scaffold and added skipping tests for ci

---
+++ packages/contracts/test/integration/Entrypoint.t.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.28;
+
+import {IntegrationBase} from "test/integration/IntegrationBase.sol";
+
+contract IntegrationEntrypoint is IntegrationBase {
+    function test_skip() public {
+        vm.skip(true);
+    }
+}
--- packages/contracts/test/unit/PrivacyPool.t.sol
+++ packages/contracts/test/unit/PrivacyPool.t.sol
@@ -1,8 +1,12 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.28;
 
-import {Entrypoint, IEntrypoint} from 'contracts/Entrypoint.sol';
-import {Test} from 'forge-std/Test.sol';
-import {IERC20} from 'forge-std/interfaces/IERC20.sol';
+import {PrivacyPool, IPrivacyPool} from "contracts/PrivacyPool.sol";
+import {Test} from "forge-std/Test.sol";
+import {IERC20} from "forge-std/interfaces/IERC20.sol";
 
-contract UnitGreeter is Test {}
+contract UnitPrivacyPool is Test {
+    function test_skip() public {
+        vm.skip(true);
+    }
+}
