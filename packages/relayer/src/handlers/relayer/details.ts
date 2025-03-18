229e1aaa15ca0df8c4c32a836896302693096372
moebius <0xmoebius@protonmail.com>
2025-03-18 11:34:55 +0100

    fix: linting

--- packages/relayer/src/handlers/relayer/details.ts
+++ packages/relayer/src/handlers/relayer/details.ts
@@ -3,7 +3,7 @@ import { DetailsMarshall } from "../../types.js";
 import { getAddress } from "viem/utils";
 import { Address } from "viem/accounts";
 import { CONFIG, getAssetConfig, getChainConfig } from "../../config/index.js";
-import { ConfigError, ValidationError } from "../../exceptions/base.exception.js";
+import { ValidationError } from "../../exceptions/base.exception.js";
 
 /**
  * Handler for the relayer details endpoint.
@@ -22,46 +22,46 @@ export function relayerDetailsHandler(
   // Get query parameters
   const chainIdParam = req.query.chainId as string | undefined;
   const assetAddressParam = req.query.assetAddress as string | undefined;
-  
+
   // Both parameters must be provided
   if (!chainIdParam) {
     throw ValidationError.invalidInput({ message: "Chain ID is required" });
   }
-  
+
   if (!assetAddressParam) {
     throw ValidationError.invalidInput({ message: "Asset address is required" });
   }
-  
+
   // Parse chain ID
   const parsedChainId = parseInt(chainIdParam, 10);
   if (isNaN(parsedChainId)) {
     throw ValidationError.invalidInput({ message: "Invalid chain ID format" });
   }
   const chainId = parsedChainId;
-  
+
   // Validate asset address format
   let normalizedAssetAddress: string;
   try {
     normalizedAssetAddress = getAddress(assetAddressParam);
-  } catch (error) {
+  } catch {
     throw ValidationError.invalidInput({ message: "Invalid asset address format" });
   }
 
   // Get chain configuration
   const chainConfig = getChainConfig(chainId);
-  
+
   // Get fee receiver address for this chain
   const feeReceiverAddress = chainConfig.fee_receiver_address || CONFIG.defaults.fee_receiver_address;
-  
+
   // Get asset configuration  
   const assetConfig = getAssetConfig(chainId, normalizedAssetAddress);
-  
+
   if (!assetConfig) {
-    throw ValidationError.invalidInput({ 
-      message: `Asset ${normalizedAssetAddress} not supported on chain ${chainId}` 
+    throw ValidationError.invalidInput({
+      message: `Asset ${normalizedAssetAddress} not supported on chain ${chainId}`
     });
   }
-  
+
   // Return details for the specific asset
   res.status(200).json(
     res.locals.marshalResponse(
@@ -74,6 +74,6 @@ export function relayerDetailsHandler(
       )
     )
   );
-  
+
   next();
 }
