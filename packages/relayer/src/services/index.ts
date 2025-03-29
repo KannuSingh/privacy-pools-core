d96547a459e26398cbbdb3456bf7fb5618efee89
Francisco Bezzecchi <bezze@fatsolutions.xyz>
2025-03-29 15:49:29 -0300

    feat: importing quote service

--- packages/relayer/src/services/index.ts
+++ packages/relayer/src/services/index.ts
@@ -1,3 +1,6 @@
 export { PrivacyPoolRelayer } from "./privacyPoolRelayer.service.js";
 import { PrivacyPoolRelayer } from "./privacyPoolRelayer.service.js";
+import { QuoteService } from "./quote.service.js";
+
 export const privacyPoolRelayer = new PrivacyPoolRelayer();
+export const quoteService = new QuoteService();
