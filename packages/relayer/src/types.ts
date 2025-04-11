0b5170e72c355580c8ea629f2bcb96ae33557cea
Francisco Bezzecchi <bezze@fatsolutions.xyz>
2025-04-11 14:40:24 -0300

    fix: solving types in DetailsMarshall

--- packages/relayer/src/types.ts
+++ packages/relayer/src/types.ts
@@ -6,42 +6,34 @@ export abstract class RelayerMarshall {
 }
 
 export class DetailsMarshall extends RelayerMarshall {
-  constructor(
-    private feeBPS: bigint,
-    private feeReceiverAddress: Address,
-    private chainId?: number,
-    private assetAddress?: Address,
-    private minWithdrawAmount?: bigint,
-    private maxGasPrice?: bigint,
-  ) {
+  constructor(private details: {
+    feeBPS: bigint,
+    feeReceiverAddress: Address,
+    chainId?: number,
+    assetAddress?: Address,
+    minWithdrawAmount?: bigint,
+    maxGasPrice?: bigint,
+  }) {
     super();
   }
   override toJSON(): object {
-    const result: Record<string, string | number | undefined | null> = {
-      feeBPS: this.feeBPS.toString(),
-      feeReceiverAddress: this.feeReceiverAddress.toString(),
-    };
-    
-    if (this.chainId !== undefined) {
-      result.chainId = this.chainId;
-    }
-    
-    if (this.assetAddress !== undefined) {
-      result.assetAddress = this.assetAddress.toString();
-    }
-    
-    if (this.minWithdrawAmount !== undefined) {
-      result.minWithdrawAmount = this.minWithdrawAmount.toString();
-    }
-    
-    if (this.maxGasPrice !== undefined) {
-      result.maxGasPrice = this.maxGasPrice.toString(10);
+
+    let maxGasPrice: string | null;
+    if (this.details.maxGasPrice !== undefined) {
+      maxGasPrice = this.details.maxGasPrice.toString(10);
     }
     else {
-      result.maxGasPrice = null;
+      maxGasPrice = null;
     }
-    
-    return result;
+
+    return {
+      feeBPS: this.details.feeBPS.toString(),
+      feeReceiverAddress: this.details.feeReceiverAddress.toString(),
+      chainId: this.details.chainId,
+      assetAddress: this.details.assetAddress?.toString(),
+      minWithdrawAmount: this.details.minWithdrawAmount?.toString(),
+      maxGasPrice
+    };
   }
 }
 
