4b9817ebe347007742c951d59e693edcc06fcb50
Francisco Bezzecchi <bezze@fatsolutions.xyz>
2025-04-11 21:21:43 -0300

    chore: removing uneeded service

--- packages/relayer/src/services/index.ts
+++ packages/relayer/src/services/index.ts
@@ -1,8 +1,6 @@
 export { PrivacyPoolRelayer } from "./privacyPoolRelayer.service.js";
 import { PrivacyPoolRelayer } from "./privacyPoolRelayer.service.js";
 import { QuoteService } from "./quote.service.js";
-import { UniswapService } from "./uniswap.service.js";
 
-export const uniswapService = new UniswapService();
 export const privacyPoolRelayer = new PrivacyPoolRelayer();
 export const quoteService = new QuoteService();
