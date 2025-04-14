49a9c1edebbd84d8b87cab3e5f548427c2204f87
Francisco Bezzecchi <bezze@fatsolutions.xyz>
2025-04-14 18:28:36 -0300

    feat: adding quote case for native asset

--- packages/relayer/src/providers/quote.provider.ts
+++ packages/relayer/src/providers/quote.provider.ts
@@ -3,20 +3,12 @@ import { uniswapProvider } from "./index.js";
 
 export class QuoteProvider {
 
-  // a typical withdrawal costs between 450k-650k gas
-  static txCost: bigint = 700_000n;
-
   constructor() {
   }
 
   async quoteNativeTokenInERC20(chainId: number, addressIn: Address, amountIn: bigint): Promise<{ num: bigint, den: bigint }> {
     const { in: in_, out } = (await uniswapProvider.quoteNativeToken(chainId, addressIn, amountIn))!;
     return { num: out.amount, den: in_.amount };
-  }
-
-  async quoteFeeBPSNative(baseFee: bigint, balance: bigint, nativeQuote: { num: bigint, den: bigint }, gasPrice: bigint, value: bigint): Promise<bigint> {
-    const nativeCosts = (1n * gasPrice * QuoteProvider.txCost + value)
-    return baseFee + nativeQuote.den * 10_000n * nativeCosts / balance / nativeQuote.num;
   }
 
 }
